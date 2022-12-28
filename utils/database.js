1e3df01f0abae9148244c4d0328c7ded48a35223
Sanjeev Dasgupta <sanjeev_dasgupta@hotmail.com>
2022-12-28 20:23:48 +0530

    Update database.js

--- utils/database.js
+++ utils/database.js
@@ -2,14 +2,14 @@ const {Sequelize} = require('sequelize');
 
 
 
-const sequelize = new Sequelize('d2mf76t33039l4', 'lchvywrsfggpww', 'bd5eaf71e64a395837e00e7352b3057715b74d9133f0514ba79bbcec509acc26', {
+const sequelize = new Sequelize('neondb', 'sanjeev.dasgupta', '6jNiyWEKxoA1', {
     dialect: 'postgres',
     dialectOptions: {
     ssl: {
         rejectUnauthorized: false
     }
 },
-    host: 'ec2-34-198-186-145.compute-1.amazonaws.com'
+    host: 'ep-solitary-meadow-593229.ap-southeast-1.aws.neon.tech'
 });
 
 /** REMOTE DEV ENV SETTINGSS
